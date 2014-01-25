3f131e7b6ed9ede47e8889dbf58782142d2424b9
Nick Zbinden <nickik@gmx.ch>
2014-01-26 00:59:20 +0100

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojit
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojit/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojit.core-test
+  (:require [clojure.test :refer :all]
+            [clojit.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
