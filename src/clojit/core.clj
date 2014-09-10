f5061a019f05fa01a0d3ed2915ea72a122f0c58c
Nick Zbinden <nickik@gmx.ch>
2014-09-10 20:33:00 +0200

    Cleanup core

--- src/clojit/core.clj
+++ src/clojit/core.clj
@@ -7,28 +7,6 @@
    [clojure.tools.analyzer :as a]))
 
 
-
-(defn read-from-file-with-trusted-contents [filename]
-  (with-open [r (java.io.PushbackReader.
-                 (clojure.java.io/reader filename))]
-    (binding [*read-eval* false]
-      (edn/read r))))
-
-(defn -main2 [clj-file]
-
-  (let [clj-form (read-from-file-with-trusted-contents clj-file)
-        clj-ast (anal/ast clj-form)
-        bc (c/c0 clj-ast)]
-    (p/pprint clj-form)
-    (p/pprint clj-ast)
-    (p/pprint bc)
-    (c/gen-bytecode-output-data bc)
-    )
-
-  #_(c/gen-file-output (c/c0 (anal/ast (read-from-file-with-trusted-contents clj-file))))
-  )
-
-
 (defn -main [clj-str]
     (let [clj-form (edn/read-string clj-str)
           clj-ast (anal/fast clj-form)
