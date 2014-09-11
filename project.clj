db8a835c989e70142c6774d3c6bed7aa9b258c85
Nick Zbinden <nickik@gmx.ch>
2014-09-11 19:19:45 +0200

    Dependency upgrades

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-		 	           [org.clojure/tools.analyzer "0.1.0-alpha2"]
-                 [org.clojure/tools.reader "0.8.3"]
-                 [org.clojure/data.json "0.2.4"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+		 	           [org.clojure/tools.analyzer "0.5.3"]
+                 [org.clojure/tools.reader "0.8.8"]
+                 [org.clojure/data.json "0.2.5"]
                  [org.clojure/tools.trace "0.7.8"]
 		             [prismatic/schema "0.2.6"]
                  ]
